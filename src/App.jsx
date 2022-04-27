dffee344556764414a06b1c903d3c3ebd76d0a38
Kseniia Kovalenko <kseniiakovalenko.94@gmail.com>
2022-04-27 15:42:46 +0300

    Add class components

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Header from "./components/Header/";
 
-function App(props) {
-  const logoText = 'MyReact';
-  return (
-    <div>
-      <Header text = {logoText}/>
-      <main>
-        <Home />
-        <About />
-      </main>
-    </div>
+class App extends React.Component {
+  render() {
+    const logoText = 'MyReact';
+    return (
+      <div>
+        <Header text = {logoText}/>
+        <main>
+          <Home />
+          <About />
+        </main>
+      </div>
   );
+  }
 }
 
 export default App;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import NavLink from './NavLink';
 
-function Nav() {
-    return (
-    <nav>
-        <ul>
-            <NavLink href="/" text="Home"></NavLink>
-            <NavLink href="/about" text="About"></NavLink>
-            <NavLink href="https://www.google.com" text="Google"></NavLink>
-        </ul>
-    </nav>
-    )
+class Nav extends React.Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <NavLink href="/" text="Home"></NavLink>
+                    <NavLink href="/about" text="About"></NavLink>
+                    <NavLink href="https://www.google.com" text="Google"></NavLink>
+                </ul>
+            </nav>
+            )
+    }
 }
 
 export default Nav;
